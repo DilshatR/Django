8a967a24249d7e1923a1b866a2d46a24d7291d4d
dilshat_r <rd.lan.1990@gmailcom>
2024-07-05 14:53:37 +0500

    Create static url

--- service/service/urls.py
+++ service/service/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+
+from service import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('fdc_corp.urls')),
-]
+] + static( settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
